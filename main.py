c6bdaa8f776c414965f900b8545bec8e744f323a
binder0802-gif <binder0802@gmail.com>
2026-01-03 21:29:13 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ Created on Sat Jan  3 21:15:03 2026
 @author: User
 """
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "server is running"}
